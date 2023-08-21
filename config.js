8a3693d208d90b8eaacbac4cfb0e9fd958ca4347
MEVTR-3D <141884864+MEVTR-3D@users.noreply.github.com>
2023-08-21 05:09:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,25 +3,25 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = "94786802371"
+global.owner = "23762084573"
 global.mongodb = process.env.MONGODB_URI || "mongodb://ugykkx9yl6qknshgcown:lJ18kzUxXP5IqyyUZZc@bdzoktb5vy7mozj8zh6k-mongodb.services.clever-cloud.com:2319/bdzoktb5vy7mozj8zh6k"
 global.port= process.env.PORT || 3000
 global.email = 'nimeshofficial.info@gmail.com'
 global.github = 'https://github.com/nimesh-official/Black-Dragon'
 global.location = 'Sri Lanka'
 global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '94786802371'
+global.sudo = process.env.SUDO || '237659074870'
 global.devs = '94786802371';
 global.website = 'https://github.com/nimesh-official/Black-Dragon'
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/17d25248a18357d93d98e.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME || 'Black Dragon',
+  botname:   process.env.BOT_NAME || 'MEVTR BOT',
   ownername: process.env.OWNER_NAME || 'Nimesh Official',
   sessionName:  process.env.SESSION_ID || 'Blackdragon;;;93QHXL7I#z7FsuiPEsD4RVBIOFELOE98SZXcwFrVGDiue3SxDz78',
-  author:  process.env.PACK_INFO === (";")[0] || 'Nimesh Official',
+  author:  process.env.PACK_INFO === (";")[0] || 'MEVTR',
   auto_read_status :  process.env.AUTO_READ_STATUS || 'false',
   packname:  process.env.PACK_INFO === (";")[1] || 'Black-Dragon',
-  autoreaction:  process.env.AUTO_REACTION || 'false',
+  autoreaction:  process.env.AUTO_REACTION || 'true',
   antibadword :  process.env.ANTI_BAD_WORD || 'nbwoed',
   alwaysonline:  process.env.ALWAYS_ONLINE || 'false',
   antifake : process.env.FAKE_COUNTRY_CODE || '971',
